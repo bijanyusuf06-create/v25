7094f56104779a79ed6d925059474bda727f2ccd
bijanyusuf06-create <bijanyusuf06@gmail.com>
2025-11-20 13:37:51 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-# Ensure LF line endings
-# Navigate to your bot directory first
-cd "$(dirname "$0")"
-
-# Activate virtual environment if you have one (optional)
-# source venv/bin/activate
-
-# Run the bot
-python3 bot.py
+python3 v25_telegram_bot_fixed.py
